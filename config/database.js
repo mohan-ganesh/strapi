9c49784886e28eb6419d2f89a0b6d77b872e54bc
Ganesh, Mohan <mohanganesh@gmail.com>
2022-12-29 20:29:08 -0500

    Local Testing Configs

---
+++ config/database.js
@@ -0,0 +1,24 @@
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+    debug: true,
+    acquireConnectionTimeout: 1000000,
+    options: {
+      pool: {
+        min: 1,
+        max: 10,
+        acquireTimeoutMillis: 900000,
+        createTimeoutMillis: 900000,
+        destroyTimeoutMillis: 900000,
+      },
+    },
+  },
+});
